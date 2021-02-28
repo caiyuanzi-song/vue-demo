9fb810f536c347e762c10afe0c4c58be5da64eb8
caiyuanzi-song <913062247@qq.com>
2021-02-28 19:00:59 +0800

    props

--- demo-02/组件间通信/components/AppHome.js
+++ demo-02/组件间通信/components/AppHome.js
@@ -3,16 +3,27 @@
         
     <!--右边上半区域-->
     <h1 class="page-header">Dashboard</h1>
-        <dashboard></dashboard>
+        <dashboard :hobbies="hobbies"></dashboard>
     <!--右边下半区域-->
         <h2 class="sub-header">Section title</h2>
-        <home-list></home-list>
+        <home-list :emp-list="empList"></home-list>
         </div>`
       window.AppHome = {
           template,
           components: {
               Dashboard,
               HomeList
-          }
+          },
+          data() {
+              return {
+                  hobbies: ['打篮球','跑步','唱歌','跳舞'],
+                  empList: [
+                      {id:1,name:'张三',age:18},
+                      {id:2,name:'李四',age:19},
+                      {id:3,name:'王五',age:20},
+                      {id:4,name:'赵六',age:21}
+                  ]
+              }
+          },
       }
 })()
--- demo-02/组件间通信/components/Dashboard.js
+++ demo-02/组件间通信/components/Dashboard.js
@@ -1,27 +1,13 @@
 ;(function(){
     const template = `<div class="row placeholders">
-      <div class="col-xs-6 col-sm-3 placeholder">
+      <div v-for="(hobby,index) in hobbies" class="col-xs-6 col-sm-3 placeholder">
         <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
-      </div>
-      <div class="col-xs-6 col-sm-3 placeholder">
-        <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
-      </div>
-      <div class="col-xs-6 col-sm-3 placeholder">
-        <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
-      </div>
-      <div class="col-xs-6 col-sm-3 placeholder">
-        <img src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
-        <h4>Label</h4>
+        <h4>{{hobby}}</h4>
         <span class="text-muted">Something else</span>
       </div>
     </div>`
     window.Dashboard = {
-        template
+        template,
+        props: ['hobbies']
     }
 })()
